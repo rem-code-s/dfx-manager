6e1f71083b14305f888bf89b8384a3b2e6f70ec1
Remco <remco@r3m.codes>
2022-12-20 09:57:29 +0100

    update name + readme

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,13 +6,13 @@
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
-            dfx_ui::logic::dfx::dfx,
-            dfx_ui::logic::projects::get_projects_path,
-            dfx_ui::logic::projects::initial_project_path_setup,
-            dfx_ui::logic::projects::create_project,
-            dfx_ui::logic::projects::get_projects_by_language,
-            dfx_ui::logic::projects::open_path,
-            dfx_ui::logic::projects::run_command,
+            dfx_manager::logic::dfx::dfx,
+            dfx_manager::logic::projects::get_projects_path,
+            dfx_manager::logic::projects::initial_project_path_setup,
+            dfx_manager::logic::projects::create_project,
+            dfx_manager::logic::projects::get_projects_by_language,
+            dfx_manager::logic::projects::open_path,
+            dfx_manager::logic::projects::run_command,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
